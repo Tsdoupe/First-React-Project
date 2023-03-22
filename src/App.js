1b6e3c777de60e3ba432d5a3c5d03e3afac07421
Tyler Doupe <tylerdoupe34@gmail.com>
2023-03-22 10:46:17 -0400

    HomepageDisplay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
 import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />}/> 
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
+      <Footer/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
