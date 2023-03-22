95d35ec65cb09e79fbe8a0208642a9c756134a6a
Tyler Doupe <tylerdoupe34@gmail.com>
2023-03-22 17:28:14 -0400

    Comments

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,18 @@
 import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
+import {Link} from 'react-router-dom';
 
 const CampsiteCard = ({campsite}) => {
-    const {image, name } = campsite;
+    const {id, image, name } = campsite;
     return (
         <Card>
-            <CardImg width = "100%" src={campsite.image} alt={campsite.name}/>
-            <CardImgOverlay>
-                <CardTitle> 
-                    {campsite.name}
-                </CardTitle>
-            </CardImgOverlay>
+            <Link to={`${id}`}>
+                <CardImg width = "100%" src={campsite.image} alt={campsite.name}/>
+                <CardImgOverlay>
+                    <CardTitle> 
+                        {campsite.name}
+                    </CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 };
